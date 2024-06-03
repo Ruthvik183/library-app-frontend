a92395da777c70f7aab5c216dec2b3a334bb533f
Ruthvik183 <ruthvik183@gmail.com>
2024-06-03 02:04:59 -0400

    feat: pagination and searching

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <HomePage />
+      {/* <HomePage /> */}
+      <SearchBooksPage />
       <Footer />
     </div>
   );
